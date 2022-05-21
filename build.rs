eff9b442edf82745aadb7979fd6231ee70fd29bc
Davide Ghilardi <ghilardi.davide@gmail.com>
2022-05-21 10:47:11 +0200

    define simple grpc protocol

---
+++ build.rs
@@ -0,0 +1,11 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    tonic_build::configure()
+        .compile(
+            &["hailstorm.proto"],
+            &["proto"],
+        )?;
+    
+    Ok(())
+}
