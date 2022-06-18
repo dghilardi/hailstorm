b478027ca93dc21214b436f08318e8eaaf09c145
Davide Ghilardi <ghilardi.davide@gmail.com>
2022-06-18 18:27:55 +0200

    add env rune extension

---
+++ src/simulation/rune/extension/env/module.rs
@@ -0,0 +1,21 @@
+use rune::{ContextError, Module};
+
+pub struct EnvModuleConf {
+    prefix: Option<String>
+}
+
+fn read_env(maybe_prefix: Option<&String>, name: &str) -> Option<String> {
+    if let Some(prefix) = maybe_prefix {
+        std::env::var(format!("{}_{}", prefix, name)).ok()
+    } else {
+        std::env::var(name).ok()
+    }
+}
+
+pub fn module(cfg: EnvModuleConf) -> Result<Module, ContextError> {
+    let mut module = Module::with_crate_item("hailstorm", &["env"]);
+
+    module.function(&["read"], move |name: &str| read_env(cfg.prefix.as_ref(), name))?;
+
+    Ok(module)
+}
