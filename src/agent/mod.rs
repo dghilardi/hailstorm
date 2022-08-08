0f8ab8fdccf0e15cc59ad1a52a5db76e61c3ce0d
Davide Ghilardi <davide.ghilardi@comelit.it>
2022-08-08 18:20:10 +0200

    add method to spawn agent in a separate thread

--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -1,3 +1,4 @@
 pub mod builder;
 pub mod actor;
 pub mod metrics;
+pub mod spawn;
---
+++ src/agent/spawn.rs
@@ -0,0 +1,30 @@
+use std::thread;
+use std::thread::JoinHandle;
+use actix::{Addr, System};
+use crate::agent::builder::{AgentBuilder, AgentRuntime};
+use crate::communication::upstream::contract::UpstreamAgentActor;
+use crate::simulation::actor::simulation::SimulationActor;
+
+pub struct AgentHandle<Upstream: UpstreamAgentActor> {
+    handle: JoinHandle<()>,
+    runtime: AgentRuntime<Upstream>,
+}
+
+impl<ContextBuilder, UpstreamCfg, DownstreamCfg> AgentBuilder<ContextBuilder, UpstreamCfg, DownstreamCfg>
+    where
+        ContextBuilder: FnOnce(Addr<SimulationActor>) -> rune::Context + Send + 'static,
+        UpstreamCfg: Send + 'static,
+        DownstreamCfg: Send + 'static,
+{
+    pub fn spawn<Upstream: UpstreamAgentActor<Config=UpstreamCfg>>(self, exit_signal: tokio::sync::oneshot::Receiver<()>) -> AgentHandle<Upstream> {
+        let (snd, rcv) = std::sync::mpsc::channel();
+        let handle = thread::spawn(|| System::new().block_on(async move {
+            snd.send(self.launch()).expect("Error sending back AgentRuntime");
+            exit_signal.await.expect("Error receiving exit_signal")
+        }));
+        AgentHandle {
+            handle,
+            runtime: rcv.recv().expect("Error receiving AgentRuntime"),
+        }
+    }
+}
