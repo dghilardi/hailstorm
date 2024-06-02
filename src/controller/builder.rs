d1464b01b53dcdb988c5fa49bc3bf3d5357f9695
Davide Ghilardi <davide.ghilardi@comelit.it>
2024-06-02 19:01:58 +0200

    documentation for controller builder

--- src/controller/builder.rs
+++ src/controller/builder.rs
@@ -3,22 +3,54 @@ use crate::communication::server::HailstormGrpcServer;
 use crate::communication::server_actor::GrpcServerActor;
 use crate::controller::controller_actor::ControllerActor;
 use crate::MultiAgentUpdateMessage;
-use actix::{Addr, AsyncContext, Context, Recipient};
+use actix::{Actor, Addr, AsyncContext, Context, Handler};
 use std::net::SocketAddr;
 use tonic::transport::server::Router;
 use tonic::transport::Server;
 
-pub struct ControllerBuilder {
-    pub metrics_storage: Recipient<MultiAgentUpdateMessage>,
+/// Struct used to build a controller instance
+pub struct ControllerBuilder<MetricsStorage> {
+    metrics_storage: MetricsStorage,
 }
 
-impl ControllerBuilder {
+impl Default for ControllerBuilder<()> {
+    fn default() -> Self {
+        Self {
+            metrics_storage: (),
+        }
+    }
+}
+
+impl<MetricsStorage> ControllerBuilder<MetricsStorage> {
+    /// Set metrics storage actor address
+    pub fn metrics_storage<MetricsStorageAct>(
+        self,
+        metrics_storage_addr: Addr<MetricsStorageAct>,
+    ) -> ControllerBuilder<Addr<MetricsStorageAct>>
+    where
+        MetricsStorageAct:
+            Actor<Context = Context<MetricsStorageAct>> + Handler<MultiAgentUpdateMessage>,
+    {
+        ControllerBuilder {
+            metrics_storage: metrics_storage_addr,
+        }
+    }
+}
+
+impl<MetricsStorageAct> ControllerBuilder<Addr<MetricsStorageAct>>
+where
+    MetricsStorageAct:
+        Actor<Context = Context<MetricsStorageAct>> + Handler<MultiAgentUpdateMessage>,
+{
+    /// Build controller app
     pub async fn build(self) -> ControllerApp {
         let controller_ctx: Context<ControllerActor> = Context::new();
         let grpc_server_ctx: Context<GrpcServerActor> = Context::new();
 
-        let controller_actor =
-            ControllerActor::new(grpc_server_ctx.address().recipient(), self.metrics_storage);
+        let controller_actor = ControllerActor::new(
+            grpc_server_ctx.address().recipient(),
+            self.metrics_storage.recipient(),
+        );
         let grpc_server_actor = GrpcServerActor::new(controller_ctx.address().recipient());
 
         let server_addr = grpc_server_ctx.run(grpc_server_actor);
@@ -36,16 +68,19 @@ impl ControllerBuilder {
     }
 }
 
+/// Controller application state
 pub struct ControllerApp {
     controller_addr: Addr<ControllerActor>,
     grpc_router: Router,
 }
 
 impl ControllerApp {
+    /// Get address to communicate with controller actor
     pub fn controller_addr(&self) -> Addr<ControllerActor> {
         self.controller_addr.clone()
     }
 
+    /// Launch the controller and expose the grpc API
     pub async fn launch(self, address: SocketAddr) {
         self.grpc_router
             .serve(address)
