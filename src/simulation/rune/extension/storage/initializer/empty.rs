84adfda4058f774be06ac9b35de64a64790f9502
Davide Ghilardi <ghilardi.davide@gmail.com>
2024-06-09 14:23:14 +0200

    documentation for EmtpyStorage and StorageInitializerRegistry

--- src/simulation/rune/extension/storage/initializer/empty.rs
+++ src/simulation/rune/extension/storage/initializer/empty.rs
@@ -2,6 +2,14 @@ use std::collections::HashMap;
 
 use super::StorageInitializerRegistry;
 
+/// A no-op storage initializer.
+///
+/// `EmptyInitializer` is an implementation of the `StorageInitializerRegistry` that returns empty
+/// data for any request. It is useful as a default or placeholder initializer in systems that either
+/// do not require initial data loading or where initial data loading is conditionally bypassed.
+///
+/// This initializer can also be used in testing environments to mock or simulate storage initialization
+/// without actual data.
 pub struct EmptyInitializer;
 
 impl StorageInitializerRegistry for EmptyInitializer {
--- src/simulation/rune/extension/storage/initializer/mod.rs
+++ src/simulation/rune/extension/storage/initializer/mod.rs
@@ -3,6 +3,48 @@ use std::collections::HashMap;
 pub mod csv;
 pub mod empty;
 
+/// A trait for initializing storage with initial values.
+///
+/// Implementors of this trait provide a mechanism to populate storage with initial data based on
+/// a given storage name and bot ID. This is useful in scenarios where bots or agents require
+/// predefined data at the start of a simulation.
+///
+/// # Methods
+///
+/// - `initial_values_for`: Given a storage name and bot ID, returns a `HashMap` containing the
+/// initial key-value pairs to populate the storage.
 pub trait StorageInitializerRegistry {
+    /// Provides initial values for a specified storage and bot ID.
+    ///
+    /// # Parameters
+    ///
+    /// - `name`: A string slice representing the name of the storage. This could correspond to different
+    /// types or categories of data needed by bots.
+    /// - `bot_id`: A 32-bit unsigned integer representing the unique identifier of the bot.
+    /// This allows for customization of initial data on a per-bot basis.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `HashMap<String, String>` containing the initial key-value pairs for the specified
+    /// storage. If no initial data is available or relevant, an empty `HashMap` may be returned.
+    ///
+    /// # Examples
+    ///
+    /// Implementors will override this method to provide specific logic for data initialization:
+    ///
+    /// ```
+    /// use std::collections::HashMap;
+    /// use hailstorm::simulation::rune::extension::storage::initializer::StorageInitializerRegistry;
+    ///
+    /// struct MyInitializer;
+    ///
+    /// impl StorageInitializerRegistry for MyInitializer {
+    ///     fn initial_values_for(&self, name: &str, bot_id: u32) -> HashMap<String, String> {
+    ///         [(String::from("foo"), String::from("bar"))]
+    ///             .into_iter()
+    ///             .collect()
+    ///     }
+    /// }
+    /// ```
     fn initial_values_for(&self, name: &str, bot_id: u32) -> HashMap<String, String>;
 }
